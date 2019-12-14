eee467f3d128ac2e22d540911120f9e886ef33b4
Daniel Mascena <danielmascena@gmail.com>
2019-12-13 21:38:21 -0300

    adding extra section; moving animate fn to index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const dateElement = document.getElementById("wedding-date");
 const presentation = document.querySelector(
   ".section-presentation .presentation"
 );
-
+const extra = document.querySelector(".info-extra .infos");
 const months = [
   "Janeiro",
   "Feveiro",
@@ -26,20 +26,44 @@ const months = [
   "Novembro",
   "Dezembro"
 ];
-const loadImages = ({ Asset }) => {
+const loadImages = assets => {
   var count = 0;
-  for (let key in Asset) {
-    let url = Asset[key].fields.file.url;
-    +key === 1
-      ? (banner.style.backgroundImage = `url('${url}')`)
-      : (images[count++].src = url);
+  for (let asset of assets) {
+    let url = asset.fields.file.url;
+    images[count++].src = url;
   }
 };
+const animateit = _ => {
+  const mainContent = document.getElementById("main-container");
+  const sections = document.getElementsByClassName("section");
+  const animatePage = _ => {
+    var curPos = mainContent.scrollTop;
+    for (let section of sections) {
+      if (section.offsetTop - window.outerHeight / 2 <= curPos) {
+        section.classList.add("animate");
+      }
+    }
+  };
+  mainContent.addEventListener("scroll", animatePage);
+};
+
+if (document.readyState === 'loading') {
+  document.addEventListener("DOMContentLoaded", animateit);
+} else {
+  animateit();
+}
 
 fetchData(({ items, includes }) => {
-  loadImages(includes);
+  const idImgPresentation = items[0].fields.imagesLandingPage.map(
+    id => id.sys.id
+  );
+  loadImages(
+    includes.Asset.filter(asset => idImgPresentation.includes(asset.sys.id))
+  );
+  banner.style.backgroundImage = `url('${includes.Asset[1].fields.file.url}')`;
   titleBanner.textContent = items[0].fields.blogText;
   presentation.textContent = items[0].fields.generalBlogText;
+  extra.textContent = items[0].fields.generalBlogText;
   var dateStr = items[0].fields.dateOfWedding;
   var date = new Date(dateStr);
   dateElement.setAttribute("datetime", dateStr);
