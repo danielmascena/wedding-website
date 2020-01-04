07299006003ac5e8561e6f10b432be0e1e6d86df
Daniel Mascena <danielmascena@gmail.com>
2020-01-03 22:12:44 -0300

    updating footer

--- docs/src.a2b27638.js
+++ docs/src.a2b27638.js
@@ -388,7 +388,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58348" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50843" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
